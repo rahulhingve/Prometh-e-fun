7647c4e073e23ed44346cfd31f6bdcd0d2e18e4d
rahulhingve <rahulpawar2001.rp@gmail.com>
2026-03-08 22:29:29 +0530

    add: Middleware

--- index.ts
+++ index.ts
@@ -2,18 +2,21 @@ console.log("Hello via Bun!");
 
 import express from 'express';
 
+import { middleware } from './middleware';
+
+
 const app = express();
+app.use(middleware);
 
+// app.use((req, res, next) => {
+//     const start = Date.now();
 
-app.use((req, res, next) => {
-    const start = Date.now();
-
-    res.on('finish', () => {
-        const duration = Date.now() - start;
-        console.log(`[Request took ${duration}ms on  Method ${req.method} with status ${res.statusCode} on Route ${req.route.path} ]`)
-    })
-    next();
-})
+//     res.on('finish', () => {
+//         const duration = Date.now() - start;
+//         console.log(`[Request took ${duration}ms on  Method ${req.method} with status ${res.statusCode} on Route ${req.route.path} ]`)
+//     })
+//     next();
+// })
 
 
 
---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import type { NextFunction, Request, Response } from "express";
+
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+
+
+    const start = Date.now();
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        console.log(`[Request took ${duration}ms on  Method ${req.method} with status ${res.statusCode} on Route ${req.route.path} ]`)
+    })
+    next();
+
+
+
+}
